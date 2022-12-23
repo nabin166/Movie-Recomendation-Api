e4a4705c078e9a8ccbb5d429ef040ba753db4dfa
nabin166 <nabinaryal830@gmail.com>
2022-12-23 11:04:00 +0545

    Consume Genre Get request

---
+++ ConsumeApi/Controllers/TestController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movie_Api.Model;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace ConsumeApi.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7021/api/Genre/");
+
+
+            //Get 
+            var task = client.GetAsync("Getallgenre");
+
+            task.Wait();
+            var result = task.Result;
+
+            if (result.IsSuccessStatusCode)
+            {
+                var resulttask = result.Content.ReadAsStringAsync();
+                resulttask.Wait();
+
+                var data = resulttask.Result;
+
+
+               // List<da> c =  JsonSerializer.Deserialize<string>(data);
+                List<Genre> UserList = JsonConvert.DeserializeObject<List<Genre>>(data);
+                ViewBag.Result = UserList;
+           
+                return View();
+            }
+
+            return View();
+        }
+    }
+}
